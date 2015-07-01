5eae38440294c20fc58989b8648c3367e963325c
jtimberman <joshua@chef.io>
2015-07-01 15:08:55 -0600

    Make it so.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+name             'pcb'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'dev@chef.io'
+license          'Apache 2 License'
+description      'Generates a Pipeline Build Cookbook for Delivery'
+version          '0.2.0'
