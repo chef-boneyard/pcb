12a957a769d9ecfe0211964a25f3dce2a910a8c2
jtimberman <joshua@chef.io>
2015-07-02 15:27:54 -0600

    Remove unneeded files/templates for build cookbooks

--- recipes/recipe.rb
+++ recipes/recipe.rb
@@ -5,23 +5,8 @@ recipe_path = File.join(cookbook_dir, "recipes", "#{context.new_file_basename}.r
 spec_helper_path = File.join(cookbook_dir, "spec", "spec_helper.rb")
 spec_path = File.join(cookbook_dir, "spec", "unit", "recipes", "#{context.new_file_basename}_spec.rb")
 
-# Chefspec
-directory "#{cookbook_dir}/spec/unit/recipes" do
-  recursive true
-end
-
-cookbook_file spec_helper_path do
-  action :create_if_missing
-end
-
-template spec_path do
-  source "recipe_spec.rb.erb"
-  helpers(ChefDK::Generator::TemplateHelper)
-  action :create_if_missing
-end
-
 # Recipe
 template recipe_path do
-  source "recipe.rb.erb"
+  source 'recipe.rb.erb'
   helpers(ChefDK::Generator::TemplateHelper)
 end
