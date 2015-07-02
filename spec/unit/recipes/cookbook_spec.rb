4bd54c3fb425f0d2d37f2ba6838fde257b07c604
jtimberman <joshua@chef.io>
2015-07-02 14:04:45 -0600

    Unit testing goodness for LICENSE file

--- spec/unit/recipes/cookbook_spec.rb
+++ spec/unit/recipes/cookbook_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe 'pcb::cookbook' do
   let(:skip_git_init) { true }
   let(:have_git) { false }
-  let(:cookbook_root) { '/var/tmp/pcb' }
+  let(:cookbook_root) { '/var/tmp' }
   let(:cookbook_name) { 'pcb' }
-  let(:license) { 'apache2' }
   let(:cookbook_path_in_git_repo?) { false }
   let(:recipe_name) { 'cookbook' }
+  let(:license) { 'apache2' }
 
   before(:each) do
     ChefDK::Generator.add_attr_to_context(:skip_git_init, cookbook_path_in_git_repo?)
@@ -18,7 +18,7 @@ describe 'pcb::cookbook' do
     ChefDK::Generator.add_attr_to_context(:license, license)
   end
 
-  context 'All is quiet' do
+  context 'Default for one of our target platforms' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(
         platform: 'ubuntu',
@@ -26,8 +26,16 @@ describe 'pcb::cookbook' do
       ).converge(described_recipe)
     end
 
-    it 'converges' do
+    it 'converges without error' do
       expect { chef_run }.to_not raise_error
+    end
+
+    # we know that if the generate command does not specify the
+    # license option, `all_rights` is used, so let's test for a
+    # non-default option like our favorite license, `apache2`. we
+    # don't need to test it twice.
+    it 'renders the LICENSE file from the correct template source' do
+      expect(chef_run).to create_template_if_missing('/var/tmp/pcb/LICENSE').with(source: 'LICENSE.apache2.erb')
     end
   end
 end
